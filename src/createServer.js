7bb81525089cca66af0ea5e9b7e09f9e26f1c20e
tiblazy <hauntedapply@gmail.com>
2025-03-22 01:21:43 -0300

    initial commit

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const createServer = () => {
-  // your code goes here
-};
+const express = require('express');
+const { userRouter } = require('./routers/user-router');
+const { expenseRouter } = require('./routers/expense-router');
+const { resetUsersService } = require('./services/user-service');
+const { resetExpensesService } = require('./services/expense-services');
+
+function createServer() {
+  const app = express();
+
+  resetUsersService();
+  resetExpensesService();
+
+  app.use(express.json());
+
+  app.use(userRouter);
+  app.use(expenseRouter);
+
+  return app;
+}
 
 module.exports = {
   createServer,
